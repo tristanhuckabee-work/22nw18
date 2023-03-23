085d1bff095cd022c51552699091283c7b34b346
tristan <thuckabee.work@gmail.com>
2023-03-23 00:06:33 -0500

    updated code

--- t2-battleship-dom/assets/js/index.js
+++ t2-battleship-dom/assets/js/index.js
@@ -1,60 +1,61 @@
 import Board from "./board.js";
 
-window.onload = makeState();
+window.onload = resetGame;
 
-function makeState() {
+function resetGame() {
   let board = new Board(); // creates a new game board
-
   makeControls();
   makeBoard(board);
 }
-function makeBoard(bClass) {
-  let grid = bClass.grid;
+
+function makeBoard(board) {
+  let gameboard = board.grid;
   let boardDiv = document.createElement('div');
   boardDiv.id = 'board';
-  grid.forEach((row, i) => {
+
+  gameboard.forEach((row, i) => {
     row.forEach((cell, j) => {
       let cellDiv = document.createElement('div');
       cellDiv.classList.add('cell');
-      cellDiv.dataset.value = cell;
-      cellDiv.addEventListener('click', e => {
-        let res = bClass.makeHit(i, j);
-        if (!res) {
-          cellDiv.classList.add('miss');
-        } else {
-          cellDiv.innerText = res;
-          cellDiv.classList.add('hit');
-          let win = bClass.isGameOver();
-          if (win) {
-            let winModal = document.querySelector('span');
-            winModal.style.display = 'inline-block';
-          }
-        }
-      })
+      cellDiv.addEventListener('click', cellClick)
 
       boardDiv.append(cellDiv);
+      function cellClick() {
+        let hit = board.makeHit(i, j);
+        if (hit) {
+          cellDiv.classList.add('hit');
+          cellDiv.innerText = cell;
+          let win = board.isGameOver();
+          if (win) {
+            let cells = document.querySelectorAll('#board > .cell');
+            cells.forEach(bCell => {
+              bCell.style.pointerEvents = 'none';
+            });
+
+            let winDiv = document.createElement('div');
+            winDiv.innerText = 'YOU WIN!'
+            document.body.append(winDiv);
+          }
+        } else {
+          cellDiv.classList.add('miss');
+        }
+      }
     })
-  });
+  })
 
   document.body.append(boardDiv);
 }
 function makeControls() {
-  let controls = document.createElement('div');
-  controls.id = 'controls';
-  let span = '<span>You Win!</span>';
   let reset = document.createElement('button');
-  reset.id = 'reset';
-  reset.innerText = 'Reset Game'
+  reset.innerText = 'Reset Game';
   reset.addEventListener('click', e => {
-    let controls = document.querySelector('#controls');
-    let board = document.querySelector('#board');
-    controls.remove();
-    board.remove();
+    let resetButton = document.querySelector('button');
+    let boardDiv = document.querySelector('#board');
+    resetButton.remove();
+    boardDiv.remove();
 
-    makeState();
-  })
+    resetGame()
+  });
 
-  controls.innerHTML = span;
-  controls.append(reset);
-  document.body.append(controls);
+  document.body.append(reset);
 }
